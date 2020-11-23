30e8b883cfee0b6fe511ec997b01d0ec810e1ed6
eGkritsis <tzenios13@gmail.com>
2020-11-23 22:13:04 +0200

    pic

--- app/HelloWorld.cpp
+++ app/HelloWorld.cpp
@@ -3,7 +3,13 @@
 
 using namespace graphics;
 
-void draw() {}
+void draw() {
+	graphics::Brush br;
+	br.fill_opacity = 1;
+	br.outline_opacity = 0;
+	br.texture = "test1.png";
+	drawRect(250, 250, 100, 100, br);
+}
 void update(float ms) {}
 
 int main() {
@@ -12,5 +18,6 @@ int main() {
 	setDrawFunction(draw);
 	setUpdateFunction(update);
 	startMessageLoop();
+	destroyWindow();
 	return 0;
 }
