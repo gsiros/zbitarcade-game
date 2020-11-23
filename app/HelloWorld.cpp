7cdda4e0082d15503d75f3ed08175724a5b791d3
gsiros <geo2001s@gmail.com>
2020-11-23 14:31:11 +0200

    Testing the sgg lib.

---
+++ app/HelloWorld.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <graphics.h>
+
+using namespace graphics;
+
+void draw() {}
+void update(float ms) {}
+
+int main() {
+
+	createWindow(500, 500, "Hello World!");
+	setDrawFunction(draw);
+	setUpdateFunction(update);
+	startMessageLoop();
+	return 0;
+}
